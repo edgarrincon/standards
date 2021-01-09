eafb3377dafb28b14278a6dcad300af5415ead9d
= <=>
2021-01-09 13:18:39 -0400

    contact test

--- src/components/ContactUs/ContacUs.js
+++ src/components/ContactUs/ContacUs.js
@@ -20,15 +20,61 @@ export default function ContactUs() {
   }
 
   return (
-    <form className="contact-form" onSubmit={sendEmail}>
-      <input type="number" name="contact_number" />
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <>
+      <div className="home__hero-section">
+        <div className="container">
+          <div
+            className="row home__hero-row"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <div className="col">
+              <form className="contact-form" onSubmit={sendEmail}>
+                <h2>CONTACT US</h2>
+
+                <input
+                  placeholder="Write your name here.."
+                  name="user_name"
+                ></input>
+
+                <input
+                  placeholder="Let us know how to contact you back.."
+                  name="user_email"
+                ></input>
+
+                <input
+                  placeholder="Write your phone number here.."
+                  name="contact_number"
+                ></input>
+
+                <input
+                  placeholder="What would you like to tell us.."
+                  name="message"
+                ></input>
+                <div className="checkbox">
+                  <input type="checkbox" name="html"></input>
+                  <input type="checkbox" name="css"></input>
+                  <input type="checkbox" name="js"></input>
+                </div>
+
+                <input className="boton" type="submit" value="Send" />
+              </form>
+            </div>
+            <div className="col">
+              <div className="home__hero-img-wrapper">
+                <div className="home__hero-img"></div>
+                <img
+                  src="https://raw.githubusercontent.com/edgarrincon/standards/5533007b7c39fb4bd147c595ccf581ed5279f318/src/images/ecommerce.svg"
+                  alt="email"
+                  className="home__hero-img"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
